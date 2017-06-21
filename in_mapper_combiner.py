e569e184442308e956e045a0cc09dd18bb1f5aee
orizhiy <sklif.85@gmail.com>
2017-06-21 21:36:24 +0300

    in_mapper_combiner_1

--- in_mapper_combiner.py
+++ in_mapper_combiner.py
@@ -2,3 +2,16 @@
 
 import sys
 
+D = dict()
+
+for line in sys.stdin:
+    key = line.strip().split('\t')
+    for key2 in key:
+        key2 = key2.strip().split(' ')
+        print(key2)
+        exist = str(key2) in D
+        if exist:
+            D[str(key2)] = D.get(str(key2)) + 1
+        else:
+            D[str(key2)] = 1
+print(D)
