46d2f7ee83d2a30caacbc94fe56ca4864de14e73
orizhiy <sklif.85@gmail.com>
2017-06-24 16:18:11 +0300

    reducer_distinct_values_v.1.py

--- reducer_distinct_values_v.1.py
+++ reducer_distinct_values_v.1.py
@@ -7,13 +7,10 @@ val = None
 for line in sys.stdin:
     line = line.strip().split('\t')
     sub = line[0].split(',')
-    if (key != sub[0] and val != sub[1]):
+    if key != sub[0] or val != sub[1]:
+        print(sub[0] + ',' + sub[1])
         key = sub[0]
         val = sub[1]
-    else:
-        print(key + ',' + val)
-        key = sub[0]
-        val = sub[1]
-print(key + ',' + val)
+# print(key + ',' + val)
 
 
