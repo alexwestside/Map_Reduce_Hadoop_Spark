8fb64b424e67918f04b036e284fef1c170ce8e96
orizhiy <sklif.85@gmail.com>
2017-06-25 18:24:18 +0300

    reducer_district_values_v2

--- reducer_2_distinct_values_v.2.py
+++ reducer_2_distinct_values_v.2.py
@@ -6,11 +6,10 @@ D = dict()
 
 for line in sys.stdin:
     line = line.strip().split('\t')
-    if line[1] is not D:
-        D[line[1]] = line[0]
+    if line[1] not in D:
+        D[line[1]] = list(line[0])
     else:
-        if D.get(str(line[1])) != line[0]:
-            D[line[1]] = D.get(str(line[1])) + 1
-print(D)
-# for i in D:
-#     print(i + '\t' + str(D.get(str(i))))
+        if line[0] not in D.get(line[1]):
+            D[line[1]].append(line[0])
+for i in D:
+    print(i + '\t' + str(len(D.get(str(i)))))
