657bf41f81374f0d236d3eba829e154240fb4b70
orizhiy <sklif.85@gmail.com>
2017-06-26 12:41:04 +0300

    mapper_cross_correlation_v2

--- mapper_cross_correlation_v2.py
+++ mapper_cross_correlation_v2.py
@@ -1,43 +1,45 @@
 #!bin/user/python
 
+from __future__ import print_function
 import sys
 
-T = tuple()
-D = dict()
-count = 1
-key = None
-j = None
-
-
-for line in sys.stdin:
-    line = line.strip().split(' ')
-    for i in line:
-        for j in line:
+for line1 in sys.stdin:
+    line1 = line1.strip().split(' ')
+    for i in line1:
+        T = tuple()
+        for j in line1:
             if i != j:
                 T += tuple((i + ',' + j).split(' '))
-for i in T:
-    line = i.split(',')
-    if key != line[0]:
-        if key:
-            sys.stdout.write(key + '\t')
-            # print(key)
-            for k in D:
-                s = k + ':' + str(D[k])
-                sys.stdout.write(',')
-            print()
+        count = 1
         D = dict()
-        key = line[0]
-        D[line[1]] = int(count)
-    else:
-        if line[1] not in D:
-            D[line[1]] = int(count)
-        else:
-            D[line[1]] += int(1)
-sys.stdout.write(key + '\t')
-for k in D:
-    if j == k:
-        sys.stdout.write(',')
-    j = k
-    sys.stdout.write(k + ':' + str(D[k]))
-
-
+        key = None
+        j = None
+        i = None
+        for i in T:
+            line = i.split(',')
+            if key != line[0]:
+                if key:
+                    sys.stdout.write(key + '\t')
+                    for k in D:
+                        s = k + ':' + str(D[k])
+                        print(s, end='')
+                        if i != key and len(D) > 1:
+                            sys.stdout.write(',')
+                            i = key
+                    print()
+                D = dict()
+                key = line[0]
+                D[line[1]] = int(count)
+            else:
+                if line[1] not in D:
+                    D[line[1]] = int(count)
+                else:
+                    D[line[1]] += int(1)
+        print(key + '\t', end='')
+        for k in D:
+            s = k + ':' + str(D[k])
+            print(s, end='')
+            if i != key and len(D) > 1:
+                sys.stdout.write(',')
+                i = key
+        print()
