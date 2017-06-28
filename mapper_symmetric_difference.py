87c2884dcc0d8111dc4f4b7d6899c95cf932c918
orizhiy <sklif.85@gmail.com>
2017-06-28 15:35:30 +0300

    mapper_symmetric_difference

--- mapper_symmetric_difference.py
+++ mapper_symmetric_difference.py
@@ -1,6 +1,19 @@
 #!bin/user/python
 
 import sys
+key = None
+val = None
 
 for line in sys.stdin:
     line = line.strip().split('\t')
+    if key != line[0]:
+        if key:
+            print(key)
+        key = line[0]
+        val = line[1]
+    else:
+        if val != line[1]:
+            key = None
+            val = None
+if key:
+    print(key)
