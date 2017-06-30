33aea0206c67b070c09195da5c5b6994add4c84a
orizhiy <sklif.85@gmail.com>
2017-06-30 12:36:07 +0300

    mapper_TF_IDF

--- mapper_TF_IDF.py
+++ mapper_TF_IDF.py
@@ -4,6 +4,7 @@ import sys
 import re
 
 for line in sys.stdin:
-    line = filter(bool, re.split((r'[^\d\w]'), line.strip('')))
-        for word in line:
+    line = re.split((r'[^\d\w]'), line.strip(''))
+    for word in line:
+        if word and word != line[0]:
             print(word + '#' + line[0] + '\t' + '1')
