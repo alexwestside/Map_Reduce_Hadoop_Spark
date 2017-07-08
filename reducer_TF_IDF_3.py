65696b4a9897ac1f81be13cf80ca6cef72be001b
orizhiy <sklif.85@gmail.com>
2017-07-08 22:22:16 +0300

    _

--- reducer_TF_IDF_3.py
+++ reducer_TF_IDF_3.py
@@ -3,19 +3,29 @@
 import sys
 doc = None
 D = dict()
-L = list()
+n = 0
 
 for line in sys.stdin:
     line = line.strip().split('\t')
     line1 = line[1].strip().split(';')
     if doc is None:
+        D = dict()
         D[line[0]] = [line1[1]]
         doc = line[0]
+        n = line1[0]
     else:
         if doc == line[0]:
             D.get(line[0]).append(line1[1])
         else:
-            for i in range(1, len(D.values()[0])):
-                pass
+            val = D.values()
+            ln = len(list(D.values())[0])
+            for i in range(0, ln if ln > 1 else 1):
+                print(list(D.keys())[0] + '#' + (str(n) if ln < 2 else str(i + 1)) + '\t' + list(val[0][i]) + '\t' + str(ln))
+            D = dict()
             D[line[0]] = [line1[1]]
-    print(D)
+            doc = line[0]
+            n = line1[0]
+val = D.values()
+ln = len(list(D.values())[0])
+for i in range(0, ln if ln > 1 else 1):
+    print(list(D.keys())[0] + '#' + (str(n) if ln < 2 else str(i + 1)) + '\t' + list(val[0][i] + '\t' + str(ln))
