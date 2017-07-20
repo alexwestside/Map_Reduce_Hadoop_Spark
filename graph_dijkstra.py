d72e2edff938af6f3e8ef45acb4f2492ed8a7509
Olexandr Rizhiy <orizhiy@student.unit.ua>
2017-07-20 14:06:50 +0300

    re

--- graph_dijkstra.py
+++ graph_dijkstra.py
@@ -10,28 +10,9 @@ n = 0
 i = 0
 j = 0
 
-data = ('3 3', 'a b 3', 'b c 2', 'c d 1', 'a b')
-# for line in sys.stdin:
-for line in data:
-    line = line.strip().split(' ')
-    if n == 0:
-        nodes = int(line[0])
-        connect = int(line[1])
-        matrix = numpy.zeros((nodes, nodes), dtype=int)
-    else:
-        if n > connect:
-            start = line[0]
-            end = line[1]
-            break
-        else:
-            if line[0] not in D:
-                D[line[0]] = i
-
-    n+=1
-
-
-# data = ("4 8", "a 2 6", "a 3 2", "a 4 10", "2 4 4", "3 a 5", "3 2 3", "3 4 8", "4 2 1", "a 4")
-# for line in sys.stdin:
+# data = ('3 3', 'a b 3', 'b c 2', 'c d 1', 'a b')
+# # for line in sys.stdin:
+# for line in data:
 #     line = line.strip().split(' ')
 #     if n == 0:
 #         nodes = int(line[0])
@@ -39,35 +20,101 @@ for line in data:
 #         matrix = numpy.zeros((nodes, nodes), dtype=int)
 #     else:
 #         if n > connect:
-#             start = int(line[0]) - 1
-#             end = int(line[1]) - 1
+#             start = line[0]
+#             end = line[1]
 #             break
-#         matrix[int(line[1]) - 1][int(line[0]) - 1] = int(line[2])
-#     n += 1
-# # print(matrix)
-# # print(start)
-# # print(end)
-# queue = list()
-# res = 0
-# while end not in queue:
-#     n = 0
-#     for i in range(nodes):
-#         if n == 0:
-#             n = matrix[i][start]
-#             tmp = i
-#         if n > matrix[i][start] and matrix[i][start] != 0 and i not in queue:
-#             n = matrix[i][start]
-#             tmp = i
-#     res += matrix[tmp][start]
-#     queue.append(start)
-#     start = tmp
-#     if start == end:
-#         break
-#     if len(queue) == nodes:
-#         print(-1)
-#         exit(0)
-# # print(queue)
+#         else:
+#             if line[0] not in D:
+#                 D[line[0]] = i
+#
+#     n+=1
+
+
+# data = ("4 8", "a 2 6", "a 3 2", "a 4 10", "2 4 4", "3 a 5", "3 2 3", "3 4 8", "4 2 1", "a 4")
+# data = ('6 8', '1 2 4', '1 3 5', '1 4 8', '2 3 6', '3 4 7', '3 2 5', '4 3 1', '5 6 1', '1 6')
+# data = ('5 0', '1 5')
+# data = ('4 2', '1 2 10', '3 4 11', '1 4')
+for line in sys.stdin:
+# for line in data:
+    line = line.strip().split(' ')
+    if n == 0:
+        nodes = int(line[0])
+        connect = int(line[1])
+        matrix = numpy.zeros((nodes, nodes), dtype=int)
+    else:
+        if n > connect:
+            start = int(line[0]) - 1
+            end = int(line[1]) - 1
+            break
+        matrix[int(line[1]) - 1][int(line[0]) - 1] = int(line[2])
+    n += 1
+# print(matrix)
+# print(start)
+# print(end)
+queue = list()
+res = 0
+while end not in queue:
+    n = 0
+    for i in range(nodes):
+        if n == 0:
+            n = matrix[i][start]
+            tmp = i
+        if n > matrix[i][start] and matrix[i][start] != 0 and i not in queue:
+            n = matrix[i][start]
+            tmp = i
+    if n == 0:
+       # print(-1)
+       sys.stdout.write(str(-1))
+       exit(0)
+    res += matrix[tmp][start]
+    queue.append(start)
+    start = tmp
+    if start == end:
+        break
+    if len(queue) == nodes:
+        # print(-1)
+        sys.stdout.write(str(-1))
+        exit(0)
+# print(queue)
 # print(res)
+sys.stdout.write(str(res))
+
+
+# n = 0
+# i = -1
+# j = 0
+# x = 0
+# D = dict()
+# data = ('4 3', 'a b 3', 'b c 2', 'c d 1', 'a d')
+# # data = ("4 8", "1 2 6", "1 3 2", "1 4 10", "2 4 4", "3 1 5", "3 2 3", "3 4 8", "4 2 1", "1 4")
+# for line in data:
+#     line = line.strip().split(' ')
+#     if n == 0:
+#         room = int(line[0])
+#         connect = int(line[1])
+#         matrix = numpy.zeros((room, room), dtype=int)
+#         print (matrix)
+#     else:
+#         if n > connect:
+#             start = line[0]
+#             end = line[1]
+#             print (start)
+#             print (end)
+#         else:
+#             if line[0] not in D:
+#                 i+=1
+#                 D[line[0]] = i
+#                 j+=1
+#             else:
+#                 i = D.get(line[0])
+#                 j += 1
+#             if line[1] not in D:
+#                 D[line[1]] = j
+#             else:
+#                 j = D.get(line[1])
+#             matrix[j][i] = int(line[2])
+#     n+=1
+# print (matrix)
 
 
 
