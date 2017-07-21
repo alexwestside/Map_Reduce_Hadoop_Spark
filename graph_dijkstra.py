49d1444c36ec8953b2dbd9c2632be3edef7a997a
Olexandr Rizhiy <orizhiy@student.unit.ua>
2017-07-21 16:23:44 +0300

    v.3

--- graph_dijkstra.py
+++ graph_dijkstra.py
@@ -4,32 +4,6 @@ import numpy
 import heapq
 import sys
 
-# D = dict()
-# L = []
-# n = 0
-# i = 0
-# j = 0
-
-# data = ('3 3', 'a b 3', 'b c 2', 'c d 1', 'a b')
-# # for line in sys.stdin:
-# for line in data:
-#     line = line.strip().split(' ')
-#     if n == 0:
-#         nodes = int(line[0])
-#         connect = int(line[1])
-#         matrix = numpy.zeros((nodes, nodes), dtype=int)
-#     else:
-#         if n > connect:
-#             start = line[0]
-#             end = line[1]
-#             break
-#         else:
-#             if line[0] not in D:
-#                 D[line[0]] = i
-#
-#     n+=1
-
-
 # data = ("4 8", "a 2 6", "a 3 2", "a 4 10", "2 4 4", "3 a 5", "3 2 3", "3 4 8", "4 2 1", "a 4")
 # data = ('6 8', '1 2 4', '1 3 5', '1 4 8', '2 3 6', '3 4 7', '3 2 5', '4 3 1', '5 6 1', '1 6')
 # data = ('5 0', '1 5')
@@ -120,12 +94,29 @@ import sys
 #
 #
 
+# def dijkstra(V, Q, matrix, res):
+#     if len(Q) == 0:
+#         print(res)
+#         exit(0)
+#     while (len(Q) != 0):
+#         start = Q.pop(0)
+#         V.append(start)
+#         for i in range(0, nodes):
+#             if matrix[i][start] != 0:
+#                 Q.append(i)
+#         dijkstra(Q[1:], V, matrix, res)
+#
+#
+#
+#
+#
+#     pass
+
+
+
 n = 0
 Q = []
 V = []
-stack = []
-L1 = []
-L = []
 tmp = 0
 res = 0
 data = ('5 10', '1 2 10', '1 3 5', '2 3 2', '2 4 1', '3 2 3', '3 4 9', '3 5 2', '4 5 4', '5 1 7', '5 4 6', '1 4')
@@ -141,36 +132,47 @@ for line in data:
             end = int(line[1]) - 1
             break
         matrix[int(line[1]) - 1][int(line[0]) - 1] = int(line[2])
-        if int(line[0]) - 1 not in stack:
-            stack.append(int(line[0]) - 1)
-        if int(line[1]) - 1 not in stack:
-            stack.append(int(line[1]) - 1)
+        # if int(line[0]) - 1 not in stack:
+        #     stack.append(int(line[0]) - 1)
+        # if int(line[1]) - 1 not in stack:
+        #     stack.append(int(line[1]) - 1)
     n += 1
 print(matrix)
 print(start)
 print(end)
-print(stack)
+# print(stack)
 V.append(start)
-while len(V) != 0:
-    if len(Q) == 0 or Q[0] not in V:
-        for i in range(0, nodes):
-            if matrix[i][start] != 0:
-                Q = list(str(i)) + Q
-        if Q[0] not in V:
-            V.append(Q[0])
-            tmp += matrix[int(Q[0])][start]
-            if str(end) in V:
-                if res == 0 or res > tmp:
-                    res = tmp
-                    V = V[:len(V) - 1]
-                    Q = Q[1:]
+for i in range(0, nodes):
+    if matrix[i][start] != 0:
+        Q.append(i)
+while len(Q) != 0:
+    if Q[0] not in V:
+        start = int(Q[0])
+        V.append(Q[0])
+        Q.remove(Q[0])
+        if end not in V:
+            for i in range(0, nodes):
+                if matrix[i][start] != 0:
+                    Q = list(str(i)) + Q
         else:
-            V = V[:len(V) - 1]
-        start = int(Q[0])
-        Q = Q[1:]
+            if res == 0 or res > tmp:
+                res = tmp
+            Q.remove(Q[0])
     else:
-        Q = Q[1:]
-        start = int(Q[0])
+        Q.remove(0)
+
+
+
+    pass
+
+
+
+# for i in range(0, nodes):
+#     if matrix[i][start] != 0:
+#         Q.append(i)
+# dijkstra(V, Q, matrix, res)
+
+
 
 
 
